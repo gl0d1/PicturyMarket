42ee66b8ee892d813ca615060c2ec0b6a3144e8c
almas avzalov <almaska17@mail.ru>
2023-01-28 01:46:15 +0300

    customize the ProfileController

--- PictureMarketWeb/Controllers/ProfileController.cs
+++ PictureMarketWeb/Controllers/ProfileController.cs
@@ -1,6 +1,48 @@
-﻿namespace PicturyMarketWeb.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PicturyMarket.Domain.ViewModels.Profile;
+using PicturyMarket.Service.Interfaces;
+
+namespace PicturyMarketWeb.Controllers
 {
-    public class ProfileController
+    public class ProfileController : Controller
     {
+        private readonly IProfileService _profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveAsync(ProfileViewModel profileViewModel)
+        {
+            ModelState.Remove("Id");
+            ModelState.Remove("UserName");
+
+            if(ModelState.IsValid )
+            {
+                var response = await _profileService.SaveAsync(profileViewModel);
+
+                if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+                {
+                    return Json(new {descriptuon = response.Description});
+                }
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        public async Task<IActionResult> DetailAsync()
+        {
+            var userName = User.Identity.Name;
+            var response = await _profileService.GetProfileAsync(userName);
+            
+            if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+
+            return View();
+        }
     }
 }
