81e5e7f3cfaed64b89ea838a379c741326f56560
almas avzalov <almaska17@mail.ru>
2023-01-27 04:41:46 +0300

    customize the UserController

--- PictureMarketWeb/Controllers/UserController.cs
+++ PictureMarketWeb/Controllers/UserController.cs
@@ -1,6 +1,69 @@
-﻿namespace PicturyMarketWeb.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using PicturyMarket.Service.Interfaces;
+using PicturyMarket.Domain.Extensions;
+using PicturyMarket.Domain.ViewModels.User;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace PicturyMarketWeb.Controllers
 {
-    public class UserController
+    public class UserController : Controller
     {
+        private readonly IUserService _userService;
+
+        public async Task<IActionResult> GetUsersAsync()
+        {
+            var response = await _userService.GetUsersAsync();
+
+            if (response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> DeleteUserAsync(int id)
+        {
+            var response = await _userService.DeleteUserAsync(id);
+
+            if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+            {
+                return RedirectToAction("GetUsers");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(UserViewModel userViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _userService.CreateAsync(userViewModel);
+
+                if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+                {
+                    return Json(new {description = response.Description});
+                }
+
+                return BadRequest(new {errorMessage = response.Description});
+            }
+            
+            var errorMessage = ModelState.Values
+                .SelectMany(v => v.Errors.Select(x => x.ErrorMessage)).ToList().Join();
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new {errorMessage});
+        }
+
+        [HttpPost]
+        public JsonResult GetRoles()
+        {
+            var types = _userService.GetRoles();
+            return Json(types.Data);
+        }
     }
 }
