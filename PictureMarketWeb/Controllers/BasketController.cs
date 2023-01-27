1b5fb0b8752183e30067ef7f9f1d0db4bc678b24
almas avzalov <almaska17@mail.ru>
2023-01-28 01:44:14 +0300

    customize the BasketController

--- PictureMarketWeb/Controllers/BasketController.cs
+++ PictureMarketWeb/Controllers/BasketController.cs
@@ -1,6 +1,40 @@
-﻿namespace PicturyMarketWeb.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PicturyMarket.Service.Interfaces;
+
+namespace PicturyMarketWeb.Controllers
 {
-    public class BasketController
+    public class BasketController : Controller
     {
+        private IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> DetailAsync()
+        {
+            var response = await _basketService.GetItemsAsync(User.Identity.Name);
+
+            if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data.ToList());
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItemAsync(int id)
+        {
+            var response = await _basketService.GetItemAsync(User.Identity.Name, id);
+
+            if(response.StatusCode == PicturyMarket.Domain.Enum.StatusCode.OK)
+            {
+                return PartialView(response.Data);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
