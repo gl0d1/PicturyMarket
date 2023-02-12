ed14fa3c736570b9787ca0cc6826e0d44512b79e
almas avzalov <almaska17@mail.ru>
2023-02-12 03:08:14 +0300

    Change migrations

--- PictureMarket.DAL/Migrations/20230212000522_AddUsers.Designer.cs
+++ PictureMarket.DAL/Migrations/20230212000522_AddUsers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PicturyMarket.DAL;
@@ -11,9 +12,10 @@ using PicturyMarket.DAL;
 namespace PicturyMarket.DAL.Migrations
 {
     [DbContext(typeof(PicturyMarketDbContext))]
-    partial class PicturyMarketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230212000522_AddUsers")]
+    partial class AddUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PictureMarket.DAL/Migrations/20230212000522_AddUsers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PicturyMarket.DAL.Migrations
+{
+    public partial class AddUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.UpdateData(
+                table: "Picturies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreate",
+                value: new DateTime(2023, 2, 12, 3, 5, 22, 367, DateTimeKind.Utc).AddTicks(8470));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name", "Password", "Role" },
+                values: new object[] { 1, "Alex", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Picturies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreate",
+                value: new DateTime(2023, 2, 12, 3, 0, 31, 527, DateTimeKind.Utc).AddTicks(3881));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name", "Password", "Role" },
+                values: new object[] { 9, "Admin", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 2 });
+        }
+    }
+}
